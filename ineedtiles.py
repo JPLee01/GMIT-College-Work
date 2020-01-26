f1548dc2d729e6489431614c0085b8831b078e30
John Paul Lee <johnpaullee@Johns-MacBook-Pro.local>
2020-01-26 16:53:53 +0000

    Added new file for calculating tiles.

---
+++ ineedtiles.py
@@ -0,0 +1,15 @@
+# Program training
+
+length = float(input("Enter room length: "))
+width = float(input("Enter room width: "))
+
+#length  = 4
+#width = 2
+
+area = length * width
+
+needed = area * 1.05
+
+print("You need", needed, "Meters squared of tiles")
+
+print('Well done!')
